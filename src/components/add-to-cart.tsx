1179c37978c72fe4712231b6c888968654db0a83
Mohammed Taqi <taqealibade@gmail.com>
2024-03-17 23:45:48 +0300

    Microfrontend Cart Using Islands(have some error)

---
+++ src/components/add-to-cart.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource react */
+
+import { addItemToCart } from '../stores/cart';
+import type { ShopItem } from '../types';
+
+export const AddToCart = ({ item }: { item: ShopItem }) => {
+	return (
+		<button className="big-link" onClick={() => addItemToCart(item)}>
+			Add To Cart
+		</button>
+		
+	);
+};
